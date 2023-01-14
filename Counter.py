7f7d00daa64fb7bdc44bdf052fb973ffbc9c75dc
RM312 <121174130+RM312@users.noreply.github.com>
2023-01-14 22:51:39 +0530

    Update Counter.py

--- Counter.py
+++ Counter.py
@@ -4,113 +4,113 @@ import time
 
 
 class Application(tk.Frame): 
-    def __init__(self, master, *args, **kwargs):
-        tk.Frame.__init__(self, master, *args, **kwargs)
-        self.master = master
-        self.running = False
-        self.time = 0
-        self.hours = 0
-        self.mins = 0
-        self.secs = 0
-        self.build_interface()
+    def __init__(nift, master, *args, **kwargs):
+        tk.Frame.__init__(nift, master, *args, **kwargs)
+        nift.master = master
+        nift.running = False
+        nift.time = 0
+        nift.hours = 0
+        nift.mins = 0
+        nift.secs = 0
+        nift.build_interface()
 
-    def build_interface(self):
-        self.time_entry = tk.Entry(self)
-        self.time_entry.grid(row=0, column=1)
+    def build_interface(nift):
+        nift.time_entry = tk.Entry(nift)
+        nift.time_entry.grid(row=0, column=1)
 
-        self.clock = tk.Label(self, text="00:00:00", font=("Courier", 20), width=10)
-        self.clock.grid(row=1, column=1, stick="S")
+        nift.clock = tk.Label(nift, text="00:00:00", font=("Courier", 20), width=10)
+        nift.clock.grid(row=1, column=1, stick="S")
 
-        self.time_label = tk.Label(self, text="hour   min   sec", font=("Courier", 10), width=15)
-        self.time_label.grid(row=2, column=1, sticky="N")
+        nift.time_label = tk.Label(nift, text="hour   min   sec", font=("Courier", 10), width=15)
+        nift.time_label.grid(row=2, column=1, sticky="N")
 
-        self.power_button = tk.Button(self, text="Start", command=lambda: self.start())
-        self.power_button.grid(row=3, column=0, sticky="NE")
+        nift.power_button = tk.Button(nift, text="Start", command=lambda: nift.start())
+        nift.power_button.grid(row=3, column=0, sticky="NE")
 
-        self.reset_button = tk.Button(self, text="Reset", command=lambda: self.reset())
-        self.reset_button.grid(row=3, column=1, sticky="NW")
+        nift.reset_button = tk.Button(nift, text="Reset", command=lambda: nift.reset())
+        nift.reset_button.grid(row=3, column=1, sticky="NW")
 
-        self.quit_button = tk.Button(self, text="Quit", command=lambda: self.quit())
-        self.quit_button.grid(row=3, column=3, sticky="NE")
+        nift.quit_button = tk.Button(nift, text="Quit", command=lambda: nift.quit())
+        nift.quit_button.grid(row=3, column=3, sticky="NE")
 
-        self.pause_button = tk.Button(self, text="Pause", command=lambda: self.pause())
-        self.pause_button.grid(row = 3,column=2, sticky = "NW")
+        nift.pause_button = tk.Button(nift, text="Pause", command=lambda: nift.pause())
+        nift.pause_button.grid(row = 3,column=2, sticky = "NW")
 
-        self.master.bind("<Return>", lambda x: self.start())
-        self.time_entry.bind("<Key>", lambda v: self.update())
+        nift.master.bind("<Return>", lambda x: nift.start())
+        nift.time_entry.bind("<Key>", lambda v: nift.update())
 
-    def calculate(self):
+    def calculate(nift):
         """time calculation"""
-        self.hours = self.time // 3600
-        self.mins = (self.time // 60) % 60
-        self.secs = self.time % 60
-        return "{:02d}:{:02d}:{:02d}".format(self.hours, self.mins, self.secs)
+        nift.hours = nift.time // 3600
+        nift.mins = (nift.time // 60) % 60
+        nift.secs = nift.time % 60
+        return "{:02d}:{:02d}:{:02d}".format(nift.hours, nift.mins, nift.secs)
 
-    def update(self):
+    def update(nift):
         """validation"""
-        self.time = int(self.time_entry.get())
+        nift.time = int(nift.time_entry.get())
         try:
-            self.clock.configure(text=self.calculate())
+            nift.clock.configure(text=nift.calculate())
         except:
-            self.clock.configure(text="00:00:00")
+            nift.clock.configure(text="00:00:00")
 
-    def timer(self):
+    def timer(nift):
         """display time"""
-        if self.running:
-            if self.time <= 0:
-                self.clock.configure(text="Time's up!")
+        if nift.running:
+            if nift.time <= 0:
+                nift.clock.configure(text="Time's up!")
             else:
-                self.clock.configure(text=self.calculate())
-                self.time -= 1
-                self.after(1000, self.timer)
+                nift.clock.configure(text=nift.calculate())
+                nift.time -= 1
+                nift.after(1000, nift.timer)
 
-    def start(self):
+    def start(nift):
         """start timer"""
         try:
-            self.time = int(self.time_entry.get())
-            self.time_entry.delete(0, 'end')
+            nift.time = int(nift.time_entry.get())
+            nift.time_entry.delete(0, 'end')
         except:
-            self.time = self.time
-        self.power_button.configure(text="Stop", command=lambda: self.stop())
-        self.master.bind("<Return>", lambda x: self.stop())
-        self.running = True
-        self.timer()
+            nift.time = nift.time
+        nift.power_button.configure(text="Stop", command=lambda: nift.stop())
+        nift.master.bind("<Return>", lambda x: nift.stop())
+        nift.running = True
+        nift.timer()
 
-    def stop(self):
+    def stop(nift):
         """Stop timer"""
-        self.power_button.configure(text="Start", command=lambda: self.start())
-        self.master.bind("<Return>", lambda x: self.start())
-        self.running = False
+        nift.power_button.configure(text="Start", command=lambda: nift.start())
+        nift.master.bind("<Return>", lambda x: nift.start())
+        nift.running = False
 
-    def reset(self):
+    def reset(nift):
         """Resets the timer to 0."""
-        self.power_button.configure(text="Start", command=lambda: self.start())
-        self.master.bind("<Return>", lambda x: self.start())
-        self.running = False
-        self.time = 0
-        self.clock["text"] = "00:00:00"
+        nift.power_button.configure(text="Start", command=lambda: nift.start())
+        nift.master.bind("<Return>", lambda x: nift.start())
+        nift.running = False
+        nift.time = 0
+        nift.clock["text"] = "00:00:00"
 
-    def quit(self):
+    def quit(nift):
         """quit the window"""
         if tk.messagebox.askokcancel("Quit", "Do you want to quit?"):
             root.destroy()
 
-    def pause(self):
+    def pause(nift):
         """Pause timer"""
-        self.pause_button.configure(text="Resume", command=lambda: self.resume())
-        self.master.bind("<Return>", lambda x: self.resume())
-        if self.running == True:
-            self.running = False
-        self.timer()
+        nift.pause_button.configure(text="Resume", command=lambda: nift.resume())
+        nift.master.bind("<Return>", lambda x: nift.resume())
+        if nift.running == True:
+            nift.running = False
+        nift.timer()
       
 
-    def resume(self):
+    def resume(nift):
         """Resume timer"""
-        self.pause_button.configure(text="Pause", command=lambda: self.pause())
-        self.master.bind("<Return>", lambda x: self.pause())
-        if self.running == False:
-            self.running = True
-        self.timer()
+        nift.pause_button.configure(text="Pause", command=lambda: nift.pause())
+        nift.master.bind("<Return>", lambda x: nift.pause())
+        if nift.running == False:
+            nift.running = True
+        nift.timer()
        
 
             
